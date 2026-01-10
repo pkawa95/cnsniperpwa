4878889244d53bee0d399802d8aee9c33ab3cf56
Piotr Kawa <pkawa95@gmail.com>
2026-01-11 00:35:31 +0100

    PWA

--- app.js
+++ app.js
@@ -694,14 +694,5 @@ if (id === "rejectedView") {
   document.addEventListener('gesturechange', e => e.preventDefault());
   document.addEventListener('gestureend', e => e.preventDefault());
 
-  // blokada double-tap zoom
-  let lastTouchEnd = 0;
-  document.addEventListener('touchend', e => {
-    const now = Date.now();
-    if (now - lastTouchEnd <= 300) {
-      e.preventDefault();
-    }
-    lastTouchEnd = now;
-  }, { passive: false });
 
 
